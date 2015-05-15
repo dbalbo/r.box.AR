22c1a91c1139f7c094cb0ef34617647c10f21466
Debbie <debbalbo@hotmail.com>
2015-05-14 22:01:46 -0700

    validates presence of name for recipe

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,1 +1,9 @@
 require('spec_helper')
+
+
+describe(Recipe)  do
+	it('validates the presence of name') do
+		recipe=Recipe.new({:name => "", :ingredients => "", :instructions => "", :rating => ""})
+		expect(recipe.save()).to(eq(false))
+	end
+end
